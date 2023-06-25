149579e396e20383871fe904085a8c1f682cde9c
Atul Panchal <83485794+Atul-Panchal@users.noreply.github.com>
2023-06-25 10:56:37 +0530

    Update Footer.jsx

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -7,11 +7,10 @@ const Footer = () => {
         <p>Contact me on social media</p>
 
         <div className="icons">
-            <a href="https://twitter.com/kumrawal" target="_blank"><i className="fab fa-twitter"></i></a>
-            <a href="https://www.instagram.com/dipanshukumrawal/" target="_blank"><i className="fab fa-instagram"></i></a>
-            <a href="https://www.facebook.com/dipu.kumrawal/" target="_blank"><i className="fab fa-facebook"></i></a>
-            <a href="mailto:dipanshukumrawal05@gmail.com" target="_blank"><i className="fas fa-mailbox"></i></a>
-            <a href="https://github.com/DipanshuKumrawal" target="_blank"><i className="fab fa-github-square"></i></a>
+            <a href="https://twitter.com/Atul_Panchal_" target="_blank"><i className="fab fa-twitter"></i></a>
+            <a href="https://www.instagram.com/panchal_atul__/" target="_blank"><i className="fab fa-instagram"></i></a>
+            <a href="mailto:panchalatul7011@gmail.com" target="_blank"><i className="fas fa-mailbox"></i></a>
+            <a href="https://github.com/Atul-Panchal" target="_blank"><i className="fab fa-github-square"></i></a>
         </div>
         <div className="cpr">
             <h6> Designed by Dipanshu</h6>
